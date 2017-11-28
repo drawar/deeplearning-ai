d1b994facb168a25c4a12c9f543695d498f5eaac
Le Van <van_igo94@yahoo.com>
2017-11-29 00:01:57 +0800

    Added templates for first course projects

---
+++ projects/1.1-logistic-regression/command.sh
@@ -0,0 +1,3 @@
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/yqwCmb4qAZk6RYY7nvNijC' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.tornado_settings="{'headers': {'Content-Security-Policy': \"frame-ancestors 'self' www.floydhub.com \"}}" --NotebookApp.iopub_data_rate_limit=1.0e10
